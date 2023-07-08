7d001cd9f7a18c699f87c3b8f5482444122c5eb5
Antoine Droyer <antoine.droyer@gmail.com>
2023-07-08 19:42:24 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import glsl from 'vite-plugin-glsl';
-import glslify from 'vite-plugin-glslify'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), glslify()],
+  plugins: [vue(), glsl()],
 })
